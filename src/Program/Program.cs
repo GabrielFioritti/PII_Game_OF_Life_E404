5585d4092221c39809f156f205506818a1b6a565
Leandro Pereira <leandronobper@gmail.com>
2024-09-03 17:29:02 -0300

    Add code that works

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
 
 namespace Ucu.Poo.GameOfLife
 {
@@ -6,7 +8,39 @@ namespace Ucu.Poo.GameOfLife
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Board board = new Board("C:\\Users\\leand\\RiderProjects\\PII_Game_OF_Life_E404\\src\\Program\\board.txt");
+
+            bool[,] b = board.GameBoard; //variable que representa el tablero
+            int width = board.BoardWith;//variabe que representa el ancho del tablero
+            int height = board.BoardHeight; //variabe que representa altura del tablero
+            
+            while (true)
+            {
+                Console.Clear();
+                StringBuilder s = new StringBuilder();
+                for (int y = 0; y<height;y++)
+                {
+                    for (int x = 0; x<width; x++)
+                    {
+                        if(b[x,y])
+                        {
+                            s.Append("|X|");
+                        }
+                        else
+                        {
+                            s.Append("___");
+                        }
+                    }
+                    s.Append("\n");
+                }
+                Console.WriteLine(s.ToString());
+                //=================================================
+                //Invocar método para calcular siguiente generación
+                //=================================================
+                board.nextGeneration();
+                b = board.GameBoard;
+                Thread.Sleep(300);
+            }
         }
     }
 }
